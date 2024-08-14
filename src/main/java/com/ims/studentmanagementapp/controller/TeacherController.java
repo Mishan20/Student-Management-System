d1237e361390727cd70390b3bb930307aa83aa12
Mishan20 <ishansenanayaka1@gmail.com>
2024-08-14 23:59:09 +0530

    Create Teacher controller and DTOs

---
+++ src/main/java/com/ims/studentmanagementapp/controller/TeacherController.java
@@ -0,0 +1,11 @@
+package com.ims.studentmanagementapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/teacher")
+public class TeacherController {
+}
---
+++ src/main/java/com/ims/studentmanagementapp/dto/TeacherDTO.java
@@ -0,0 +1,16 @@
+package com.ims.studentmanagementapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TeacherDTO {
+
+    private int teacherid;
+    private String teachername;
+    private String address;
+    private String phone;
+}
